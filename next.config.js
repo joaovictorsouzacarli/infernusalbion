7411395f32796db5352f96fe3e5d07679039f825
João Victor <joao.carli@unesp.br>
2025-03-13 11:52:51 -0300

    Adicionando meu projeto ao repositório08

--- next.config.js
+++ next.config.js
@@ -1,16 +1,31 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    typescript: {
-      // !! ATENÇÃO: Isso ignora erros de tipo durante o build
-      ignoreBuildErrors: true,
-    },
-    eslint: {
-      // !! ATENÇÃO: Isso ignora erros de lint durante o build
-      ignoreDuringBuilds: true,
-    },
-  }
-  
-  module.exports = nextConfig
-  
-  
+  reactStrictMode: false,
+  typescript: {
+    // Ignorar erros de TypeScript durante o build
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    // Ignorar erros de ESLint durante o build
+    ignoreDuringBuilds: true,
+  },
+  // Ignorar erros de análise de código durante o build
+  swcMinify: true,
+  experimental: {
+    // Desativar verificações experimentais
+    esmExternals: "loose",
+  },
+  // Aumentar o limite de tamanho do bundle
+  webpack: (config, { isServer }) => {
+    // Aumentar o limite de tamanho do bundle
+    config.performance = {
+      hints: false,
+      maxEntrypointSize: 512000,
+      maxAssetSize: 512000,
+    }
+    return config
+  },
+}
+
+module.exports = nextConfig
+
